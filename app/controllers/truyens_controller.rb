62ec1a43ce8a6e4114f57c84e51fb3e162373bf2
lumosnysm <lumosnysm@gmail.com>
2017-05-02 09:52:09 +0700

    sua giao dien danh gia

--- app/controllers/truyens_controller.rb
+++ app/controllers/truyens_controller.rb
@@ -10,9 +10,22 @@ class TruyensController < ApplicationController
     if @truyen.ratings.exists?(:user_id => current_user.try(:id))
       @rated=true
       @user_rate= @truyen.ratings.find_by_user_id(current_user.id).rate
+      @Is_user_rate_int=(@user_rate -@user_rate.to_i)>0?false : true
+      @count1=@user_rate.to_i
+      if @Is_user_rate_int
+        @count1=@user_rate
+      end
     end
+
     @rating_num=@truyen.ratings.count
     @average_rate=@truyen.ratings.average(:rate)
+
+    @Is_average_rate_int=(@average_rate -@average_rate.to_i)>0?false : true
+    @count=@average_rate.to_i
+    if @Is_average_rate_int
+      @count=@average_rate
+    end
+
   end
   def all
     @theloais=Theloai.all
