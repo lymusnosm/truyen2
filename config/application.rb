98380bd4b59bebdee9ef66550c0cf4cda9a4c8fd
Ngô Hải Yến <yourgod1989@gmail.com>
2017-05-26 14:38:17 +0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Truyensongngu7
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
