5d352fe5eb753e77530145439399a3e7485cb6aa
Ngô Hải Yến <yourgod1989@gmail.com>
2017-05-27 23:29:27 +0700

    fix

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -1,12 +1,12 @@
 class NewController < ApplicationController
   def show
-  @theloais=Theloai.all
-  @truyens=Truyen.order("updated_at DESC")
+	  @theloais=Theloai.all
+	  @truyens=Truyen.order("updated_at DESC")
 
-  # CODE LAY DU LIEU RATE TUNG TRUYEN ( CODE NAY DI KEM CODE TRONG view)
-  @average_rate=0
-  @Is_average_rate_int=true
-  @count=0
+	  # CODE LAY DU LIEU RATE TUNG TRUYEN ( CODE NAY DI KEM CODE TRONG view)
+	  @average_rate=0
+	  @Is_average_rate_int=true
+	  @count=0
 
-end
+  end
 end
